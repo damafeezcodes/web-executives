74200edcf612ff79761adf8c67fbd9965bb259b5
Afeez Awoyemi <afeezcodes@gmail.com>
2017-08-20 15:13:09 +0100

    heroku port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 app.get('/', (req,res)=>{
