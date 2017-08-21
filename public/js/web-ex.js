3d0f62a68a89f7e64518cdf92915e1a7d9482c68
Afeez Awoyemi <afeezcodes@gmail.com>
2017-08-21 16:04:21 +0100

    Second commit on 21st August

--- public/js/web-ex.js
+++ public/js/web-ex.js
@@ -9,9 +9,30 @@ $(document).ready(function(){
         $('header .side-bar').removeClass('z-depth-2');
         
     });
-    
-});
+    // var showcaseText = $('.showcase-text');  
+    // console.log(showcaseText);
+    // for(var i = 0; i<showcaseText.length; i++){
+    //     console.log(showcaseText.offset().top);
+    // }
+    // $(window).scroll(function(){
+    //     var pos
+    // })
+    // if(pos < 400){
+    //     var bendRotateAngle = (- ((pos/400) * 25) + 25);
+    //     $('.bend').css('transform', 'rotate(-' + bendRotateAngle + 'deg)');
+    //     $('.bend').show();
+    //   }else{
+    //     $('.bend').css('transform', 'rotate(-' + 0 + 'deg)');
+    //     $('.bend').hide();
+    //   }
+    //     if(pos > 170){
+    //       $('.scroll-down-btn').fadeOut(1000);
+    //     }else{
+    //       $('.scroll-down-btn').fadeIn(0);
+    //     }
+      
+    });
+  
 AOS.init({
-    easing: 'ease-in-sine',
-    duration: 300,
+    easing: 'ease-in-out-sine'
 });
