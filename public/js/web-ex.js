3817ac2531e398470440701451045b5dcf643012
Afeez Awoyemi <afeezcodes@gmail.com>
2017-08-22 10:15:06 +0100

    First Commit on 22, August

--- public/js/web-ex.js
+++ public/js/web-ex.js
@@ -9,14 +9,17 @@ $(document).ready(function(){
         $('header .side-bar').removeClass('z-depth-2');
         
     });
-    // var showcaseText = $('.showcase-text');  
-    // console.log(showcaseText);
-    // for(var i = 0; i<showcaseText.length; i++){
-    //     console.log(showcaseText.offset().top);
-    // }
-    // $(window).scroll(function(){
-    //     var pos
-    // })
+    var showcaseText = document.getElementsByClassName('showcase-text');
+    $(window).scroll(function(){
+        var pos = $(window).scrollTop();
+        for(var i = 0; i<showcaseText.length; i++){
+            if(pos > showcaseText[i].offsetTop){
+                console.log(`div ${i} position: ${showcaseText[i].offsetTop}`);
+            }
+
+        }
+        console.log(`scrollpos: ${pos}`); 
+    });
     // if(pos < 400){
     //     var bendRotateAngle = (- ((pos/400) * 25) + 25);
     //     $('.bend').css('transform', 'rotate(-' + bendRotateAngle + 'deg)');
